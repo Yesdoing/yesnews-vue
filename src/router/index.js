b7db5a61f773e6d4efbe25eb31951fde4682ffff
이장희 <janghee.lee@gooroomee.com>
2019-10-29 12:47:28 +0900

    feat: add router and toolbar component

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,17 @@ import VueRouter from 'vue-router';
 import NewsView from '../views/NewsView.vue';
 import AskView from '../views/AskView.vue';
 import JobsView from '../views/JobsView.vue';
-
+import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   routes: [
+    {
+      path: '/',
+      redirect: '/news',
+    },
     {
       path: '/news',
       component: NewsView,
@@ -21,5 +26,13 @@ export const router = new VueRouter({
       path: '/jobs',
       component: JobsView,
     },
+    {
+      path: '/user',
+      component: UserView
+    },
+    {
+      path: '/item',
+      component: ItemView
+    }
   ],
 });
