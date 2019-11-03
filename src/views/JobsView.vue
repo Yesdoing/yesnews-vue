ac3d976c96e96da67e6a97f04a73ab52cb8efaab
Jane Lee <Yesdoing13@gmail.com>
2019-11-03 16:33:20 +0900

    feat: add style JobsView

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
-    <p v-for="job in this.$store.state.jobs">
+    <ul class="jobs-list">
+      <li v-for="(job, index) in this.$store.state.jobs" :key="index" class="post">
+        <div>
+          <p class="jobs-title">
+            <a :href="job.url">{{ job.title }}</a>
+          </p>
+          <small class="link-text">
+            {{ job.time_ago }}, 
+            <router-link :to="`/user/${job.user}`" class="link-text">
+              {{ job.domain }}
+            </router-link>
+          </small>    
+        </div>
+      </li>
+    </ul>
+    <!-- <p v-for="job in this.$store.state.jobs">
       <a :href="job.url">{{ job.title }}</a>
       <small>{{ job.time_ago }}, {{ job.domain }}</small>
-    </p>
+    </p> -->
   </div>
 </template>
 
@@ -15,6 +30,26 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.jobs-list {
+  padding: 0;
+  margin: 0;
+}
 
+.post {
+  display: flex;
+  align-items: center;
+  list-style: none;
+  border-bottom: 1px solid #eee;
+  height: 60px;
+  padding-left: 80px;
+}
+
+.jobs-title {
+  margin: 0;
+}
+
+.link-text {
+  color: #828282;
+}
 </style>
