378e12ba2a7dd258401a5c73e613cd6c1c0c7b6b
Jane Lee <Yesdoing13@gmail.com>
2019-11-03 19:14:29 +0900

    feat: add user profile component

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="user-container">
+    <div>
+      <i class="fas fa-user"></i>
+    </div>
+    <div class="user-description">
+      <div>
+        {{ userInfo.id }}
+      </div>
+      <!-- <router-link :to="`/user/${user.id}`">{{ user.user}}</router-link> -->
+      <div class="time">{{ userInfo.created }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
-    <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <user-profile></user-profile>
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile';
 
 export default {
-  computed: {
-    userInfo() {
-      return this.$store.state.user;
-    }
+  components: {
+    UserProfile
   },
+  // computed: {
+  //   userInfo() {
+  //     return this.$store.state.user;
+  //   }
+  // },
   created() {
     const userName = this.$route.params.id;
-
     this.$store.dispatch('FETCH_USER', userName);
   },
   
