f4bca524a4a92b4269d54b55dd4506b0978d8c2b
Yesdoing <fkrmtleo12@gmail.com>
2019-11-04 20:32:52 +0900

    feat: add styling component

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -2,8 +2,9 @@
   <div>
     <section>
       <user-profile :info="fetchedItem">
-        <div slot="username">{{ fetchedItem.user }}</div>
-        <template slot="time">{{ fetchedItem.time_ago }}</template>
+        <!-- <div slot="username">{{ fetchedItem.user }}</div> -->
+        <router-link slot="username" :to="`/user/${fetchedItem.user}`">{{ fetchedItem.user}}</router-link>
+        <template slot="time">{{'Posted ' +  fetchedItem.time_ago }}</template>
       </user-profile>
       <!-- <div class="user-container">
         <div>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -2,8 +2,8 @@
   <div>
     <user-profile>
       <div slot="username">{{ userInfo.id }}</div>
-      <template slot="time">{{ userInfo.created }}</template>
-      <div slot="karma">{{ userInfo.karma }}</div>
+      <span slot="time">{{'Joined ' + userInfo.created }},  </span>
+      <span slot="karma">{{ userInfo.karma }}</span>
     </user-profile>
   </div>
 </template>
