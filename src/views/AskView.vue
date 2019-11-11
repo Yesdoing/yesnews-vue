34670c22a6d0e4c16494302cc8207433d53847a0
Jane Lee <Yesdoing13@gmail.com>
2019-11-11 23:13:05 +0900

    feat: add mixin component

---
+++ src/views/AskView.vue
@@ -0,0 +1,19 @@
+<template>
+  <list-item></list-item>
+</template>
+
+<script>
+import ListItem from '../components/ListItem';
+import ListMixin from '../mixins/ListMixin';
+
+export default {
+  components: {
+    ListItem
+  },
+  mixins: [ListMixin]
+}
+</script>
+
+<style>
+
+</style>
